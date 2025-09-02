807f8713b310bc9368cbf826eaf7144c06a162be
Eunchae <13rladmsco@gmail.com>
2025-09-02 21:38:57 +0900

    #21 [Make] 푸터 한번 더 수정합니다

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,6 +5,7 @@ export default function Footer() {
     <footer className="mt-20 border-t bg-white">
       <Container className="py-8 text-sm text-gray-600">
         <div>© {new Date().getFullYear()} Sturoom · 학습은 역시 Sturoom.</div>
+        <div>한림대학교 소프트웨어학부 캡스톤 디자인</div>
       </Container>
     </footer>
   );
