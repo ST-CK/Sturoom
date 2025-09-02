50811db8079c71268b861c520b892607e4bb95ca
지은 <20235272@hallym.ac.kr>
2025-09-02 20:55:37 +0900

    #13 [Make] Footer 글 추가  (#13)
    * #8 [Make] Footer 코드 수정
    
    * #11 [Make] Footer 글 추가
    
    ---------
    
    Co-authored-by: Choijieun <jieun@your-email.com>

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <footer className="mt-20 border-t bg-white">
       <Container className="py-8 text-sm text-gray-600">
-        © {new Date().getFullYear()} Sturoom · 학습은 역시 Sturoom.
+        <div>© {new Date().getFullYear()} Sturoom · 학습은 역시 Sturoom.</div>
+        <div className="mt-2"> 김은채, 김찬영, 최지은 </div>
       </Container>
     </footer>
   );
