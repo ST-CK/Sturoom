7d16413e8d533bfb4f6476f56ae5657879bafc03
Eunchae <13rladmsco@gmail.com>
2025-09-02 21:28:18 +0900

    #20 [Make] 푸터 이름 제거 (#20)

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,6 @@ export default function Footer() {
     <footer className="mt-20 border-t bg-white">
       <Container className="py-8 text-sm text-gray-600">
         <div>© {new Date().getFullYear()} Sturoom · 학습은 역시 Sturoom.</div>
-        <div className="mt-2"> 김은채, 김찬영, 최지은 </div>
       </Container>
     </footer>
   );
