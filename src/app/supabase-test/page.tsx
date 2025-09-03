dd510873f5ce8e6fd896b7fd3aa7c270398dc040
CHANYOUNGKIM <128570803+chanyoung1256@users.noreply.github.com>
2025-09-03 16:24:50 +0900

    #32 [Test] 테스트 (#32)

--- src/app/supabase-test/page.tsx
+++ src/app/supabase-test/page.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-// 환경변수로부터 브라우저 클라이언트 생성
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
