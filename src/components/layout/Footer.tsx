65828fff97278d14d4ad6036c61f07dda6b66b5d
Choijieun <jieun@your-email.com>
2025-09-04 17:43:24 +0900

    #41 [Make] Footer 수정 재업로드

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -45,7 +45,7 @@ export default function Footer() {
         {/* 하단 회사 정보 & 소셜 아이콘 */}
         <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4 border-t pt-4">
           <div>
-            <div className="font-bold text-[#0673ca] mb-1">Sturoom | 개인정보처리방침 | 이용약관 | We Are Hiring</div>
+            <div className="font-bold text-[#0673ca] mb-1">Sturoom | 개인정보처리방침 | 이용약관 </div>
             <div>
               ƧTΛCK³ | 김은채, 김찬영, 최지은 | 2025년도 2학기 소프트웨어 캡스톤 디자인 <br />
               ©Sturoom. ALL RIGHTS RESERVED
