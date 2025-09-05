e204ccae6fc0f5d7fe7dab3a2541dbe370a6417b
Eunchae <13rladmsco@gmail.com>
2025-09-05 16:24:56 +0900

    #45 [Design] 로그인, 회원가입 창 수정

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,15 @@
+import Container from "@/components/layout/Container";
 import Signup from "@/components/auth/Signup";
 
 export default function SignupPage() {
-  return <Signup />;
+  return (
+    <main className="bg-gray-50">
+      <Container>
+        {/* 헤더/푸터 사이 동일 간격 확보 + 중앙 정렬 */}
+        <div className="min-h-[70vh] py-16 flex items-center justify-center">
+          <Signup />
+        </div>
+      </Container>
+    </main>
+  );
 }
